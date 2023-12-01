1da9072c8095f66fcfb45c96e94308bd71f46e14
KPorus <designerfardin2021@gmail.com>
2023-12-01 20:05:57 +0600

    modified side bar

--- src/components/Layout/SideBar/SideBar.tsx
+++ src/components/Layout/SideBar/SideBar.tsx
@@ -6,9 +6,11 @@ const SideBar = ({ children }: { children: React.ReactNode }) =>
     const [toggle, setToggle] = useState(false);
     return (
         <div className='relative flex'>
-            <div className={`flex transition-all bg-[#001529] ${toggle ? "w-[8%]" : "w-[8%] md:w-[15%]"} h-[100vh] justify-center`}>
+            <div
+                className={`flex transition-all bg-[#001529] ${toggle ? "w-[8%]" : "w-[8%] md:w-[15%]"
+                    } min-h-[100vh] justify-center`}>
                 <div className='flex flex-col justify-between items-center'>
-                    <div className='flex justify-center items-center flex-col py-5'>
+                    <div className='fixed flex justify-center items-center flex-col py-5'>
                         <Link
                             href={"/"}
                             className={`link link-body py-[0.6rem] text-center ${toggle ? "w-[3rem]" : "w-[3rem] md:w-[9rem]"
@@ -22,16 +24,16 @@ const SideBar = ({ children }: { children: React.ReactNode }) =>
                                 priority={true}
                                 alt='Dashboard'
                             />
-                            <h4 className={`${toggle ? "hidden" : "hidden md:block"}`}>Dashoard</h4>
+                            <h4 className={`${toggle ? "hidden" : "hidden md:block"}`}>
+                                Dashoard
+                            </h4>
                         </Link>
                         <Link
                             href={"/Users"}
                             className={`link link-body py-[0.6rem] text-center ${toggle ? " w-[3rem]" : "w-[3rem] md:w-[9rem]"
                                 } rounded-md flex items-center justify-center gap-2`}>
                             <Image
-                                src={
-                                    "https://img.icons8.com/?size=512&id=108652&format=png"
-                                }
+                                src={"https://img.icons8.com/?size=512&id=108652&format=png"}
                                 width={40}
                                 height={40}
                                 priority={true}
@@ -43,14 +45,14 @@ const SideBar = ({ children }: { children: React.ReactNode }) =>
                         </Link>
                     </div>
                     <button
-                        className={`text-center ${toggle ? "w-[3rem]" : "w-[3rem] md:w-[9rem]"} p-2 flex items-center justify-center text-2xl font-bold bg-[#1f3f5c]`}
-                        onClick={() => setToggle(!toggle)}
-                    >
+                        className={`fixed bottom-0 text-center ${toggle ? "w-[3rem]" : "w-[3rem] md:w-[9rem]"
+                            } p-2 flex items-center justify-center text-2xl font-bold bg-[#1f3f5c]`}
+                        onClick={() => setToggle(!toggle)}>
                         {toggle ? ">" : "<"}
                     </button>
                 </div>
             </div>
-            <div className="bg-white w-full">{children}</div>
+            <div className='bg-white w-full'>{children}</div>
         </div>
     );
 };
